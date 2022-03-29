7d8d46ee0d3f6513bd71504bd88965db9661a0dc
«Artem» <art.cherepan@gmail.com>
2022-03-29 09:14:38 +0900

    Create fixtures

---
+++ src/Fixture/ClientFixture.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Fixture;
+
+use App\Domain\Booking\Entity\Client;
+use App\Domain\Booking\Entity\ValueObject\ClientName;
+use App\Domain\Booking\Entity\ValueObject\ClientPhoneNumber;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Uid\Uuid;
+
+class ClientFixture extends Fixture
+{
+    private const REFERENCE = 'client';
+
+    public function load(ObjectManager $manager): void
+    {
+        $client = new Client(Uuid::v4(), new ClientName('Иван'), new ClientPhoneNumber('8914999999'));
+
+        $manager->persist($client);
+        $manager->flush();
+
+        $this->addReference(self::REFERENCE, $client);
+    }
+}
