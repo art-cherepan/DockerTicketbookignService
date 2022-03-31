3120659403406262feefb72ae6d6e8fd0ac5785d
«Artem» <art.cherepan@gmail.com>
2022-03-31 15:44:18 +0900

    Create client and ticket in CommandHandler

--- src/Domain/Booking/Command/Handler/BookTicketCommandHandler.php
+++ src/Domain/Booking/Command/Handler/BookTicketCommandHandler.php
@@ -3,13 +3,19 @@
 namespace App\Domain\Booking\Command\Handler;
 
 use App\Domain\Booking\Command\BookTicketCommand;
+use App\Domain\Booking\Entity\ValueObject\ClientName;
+use App\Domain\Booking\Entity\ValueObject\ClientPhoneNumber;
+use App\Domain\Booking\Repository\ClientRepository;
+use App\Domain\Booking\Repository\SessionRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class BookTicketCommandHandler implements MessageHandlerInterface
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private ManagerRegistry $managerRegistry,
     ) {}
 
     /**
@@ -17,12 +23,22 @@ class BookTicketCommandHandler implements MessageHandlerInterface
      */
     public function __invoke(BookTicketCommand $command)
     {
-        $session = $command->getSession();
-        $client = $command->getClient();
-        $ticket = $command->getTicket();
+        $sessionRepository = new SessionRepository($this->managerRegistry);
+        $clientRepository = new ClientRepository($this->managerRegistry);
+
+        $sessionUuid = $command->getSessionUuid();
+        $clientName = $command->getClientName();
+        $clientPhoneNumber = $command->getClientPhoneNumber();
+
+        $clientNameDBType = new ClientName($clientName);
+        $clientPhoneNumberDBType = new ClientPhoneNumber($clientPhoneNumber);
+
+        $session = $sessionRepository->findById($sessionUuid)[0];
+
+        $ticket = $session->getFreeTicket();
+
+        $client = $clientRepository->getClient($clientNameDBType, $clientPhoneNumberDBType);
 
         $session->bookTicket($client, $ticket);
-
-        $this->entityManager->flush();
     }
 }
