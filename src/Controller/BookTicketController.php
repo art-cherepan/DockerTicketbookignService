1bdf4b65af9b5390bdfe27fe1f669006490cd20e
«Artem» <art.cherepan@gmail.com>
2022-03-31 20:28:03 +0900

    Modify BookTicketController.php -> pass $BookTicketCommand in Form

--- src/Controller/BookTicketController.php
+++ src/Controller/BookTicketController.php
@@ -4,36 +4,28 @@ namespace App\Controller;
 
 use App\Domain\Booking\Command\BookTicketCommand;
 use App\Domain\Booking\Repository\SessionRepository;
-use App\Form\InputDataForm;
+use App\Form\BookTicketForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Uid\Uuid;
 
 class BookTicketController extends AbstractController
 {
     #[Route('/main', name: 'main')]
-    public function book(
-        Request $request,
-        MessageBusInterface $commandBus,
-        SessionRepository $sessionRepository,
-    ): Response {
-        $sessionsInfo = InputDataForm::getSessionsInfo($sessionRepository);
+    public function bookTicket(Request $request, MessageBusInterface $commandBus, SessionRepository $sessionRepository): Response
+    {
+        $sessions = $sessionRepository->findAll();
 
-        $form = $this->createForm(InputDataForm::class, $sessionsInfo);
+        $command = new BookTicketCommand();
+
+        $form = $this->createForm(BookTicketForm::class, $command, ['sessions' => $sessions]);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $formData = $form->getData();
-
-            $sessionUuid = new Uuid($formData['Session']);
-
-            $clientName = (string) $formData['Name'];
-            $phoneNumber = (string) $formData['Phone'];
-
-            $commandBus->dispatch(new BookTicketCommand($sessionUuid, $clientName, $phoneNumber));
+            $commandBus->dispatch($command);
         }
 
         return $this->render('index.html.twig', [
