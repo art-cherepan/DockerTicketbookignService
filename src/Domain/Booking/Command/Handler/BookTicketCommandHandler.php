d2e974b95e30cf0ba3e85effe676aaac8eee6a27
«Artem» <art.cherepan@gmail.com>
2022-03-31 20:23:17 +0900

    Modify BookTicketCommandHandler.php -> create $clientNameDBType Modify BookTicketCommandHandler.php -> create $clientPhoneNumberDBType Modify BookTicketCommandHandler.php -> call $session->getFreeTicket()

--- src/Domain/Booking/Command/Handler/BookTicketCommandHandler.php
+++ src/Domain/Booking/Command/Handler/BookTicketCommandHandler.php
@@ -6,7 +6,6 @@ use App\Domain\Booking\Command\BookTicketCommand;
 use App\Domain\Booking\Entity\ValueObject\ClientName;
 use App\Domain\Booking\Entity\ValueObject\ClientPhoneNumber;
 use App\Domain\Booking\Repository\ClientRepository;
-use App\Domain\Booking\Repository\SessionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
@@ -23,17 +22,15 @@ class BookTicketCommandHandler implements MessageHandlerInterface
      */
     public function __invoke(BookTicketCommand $command)
     {
-        $sessionRepository = new SessionRepository($this->managerRegistry);
         $clientRepository = new ClientRepository($this->managerRegistry);
 
-        $sessionUuid = $command->getSessionUuid();
-        $clientName = $command->getClientName();
-        $clientPhoneNumber = $command->getClientPhoneNumber();
+        $clientName = $command->clientName;
+        $clientPhoneNumber = $command->clientPhoneNumber;
 
         $clientNameDBType = new ClientName($clientName);
         $clientPhoneNumberDBType = new ClientPhoneNumber($clientPhoneNumber);
 
-        $session = $sessionRepository->findById($sessionUuid)[0];
+        $session = $command->session;
 
         $ticket = $session->getFreeTicket();
 
