444b700b88fd445b226ce5ea9ac55bbdd7da6fe8
«Artem» <art.cherepan@gmail.com>
2022-04-01 11:36:39 +0900

    Remove second session fixture

--- src/Fixture/SessionFixture.php
+++ src/Fixture/SessionFixture.php
@@ -10,34 +10,23 @@ use Symfony\Component\Uid\UuidV4;
 
 class SessionFixture extends Fixture
 {
-    private const REFERENCE_SESSION_FIRST = 'sessionFirst';
-    private const REFERENCE_SESSION_SECOND = 'sessionSecond';
+    private const REFERENCE_SESSION = 'session';
 
     public function load(ObjectManager $manager): void
     {
+        $ticketCount = 10;
+
         $id = new UuidV4();
         $filmName = 'Веном 1';
-        $ticketCount = 20;
         $date = new DateTimeImmutable('2022-04-01 00:00:00');
         $timeStart = new DateTimeImmutable('2022-04-01 00:00:00');
         $timeEnd = new DateTimeImmutable('2022-04-01 22:30:00');
 
-        $sessionFirst = new Session($id, $filmName, $date, $timeStart, $timeEnd, $ticketCount);
+        $session = new Session($id, $filmName, $date, $timeStart, $timeEnd, $ticketCount);
 
-        $id = new UuidV4();
-        $filmName = 'Веном 2';
-        $ticketCount = 30;
-        $date = new DateTimeImmutable('2022-04-02 00:00:00');
-        $timeStart = new DateTimeImmutable('2022-04-02 00:00:00');
-        $timeEnd = new DateTimeImmutable('2022-04-02 22:30:00');
-
-        $sessionSecond = new Session($id, $filmName, $date, $timeStart, $timeEnd, $ticketCount);
-
-        $manager->persist($sessionFirst);
-        $manager->persist($sessionSecond);
+        $manager->persist($session);
         $manager->flush();
 
-        $this->addReference(self::REFERENCE_SESSION_FIRST, $sessionFirst);
-        $this->addReference(self::REFERENCE_SESSION_SECOND, $sessionSecond);
+        $this->addReference(self::REFERENCE_SESSION, $session);
     }
 }
