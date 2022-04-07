8859571287acc5c22937cd6fb86c5ce58182e13b
artem <art.cherepan@gmail.com>
2022-04-07 18:17:53 +0900

    Create unit tests for Session entity

---
+++ tests/Unit/SessionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Domain\Booking\Entity\Client;
+use App\Domain\Booking\Entity\Session;
+use App\Domain\Booking\Entity\Ticket;
+use DateTimeImmutable;
+use Doctrine\Common\Collections\ArrayCollection;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Uid\UuidV4;
+
+class SessionTest extends TestCase
+{
+    private Session $session;
+
+    public function setUp(): void
+    {
+        $this->session = new Session(
+            new UuidV4(),
+            'Веном 1',
+            new DateTimeImmutable('2022-04-01'),
+            new DateTimeImmutable('2022-04-01 20:00:00'),
+            new DateTimeImmutable('2022-04-01 22:30:00'),
+            20,
+        );
+    }
+
+    public function testCreatingTickets(): void
+    {
+        $tickets = $this->session->getTickets();
+
+        $this->assertInstanceOf(ArrayCollection::class, $tickets);
+    }
+
+    public function testGetCountTickets(): void
+    {
+        $tickets = $this->session->getTickets();
+
+        $this->assertEquals(20, count($tickets));
+    }
+
+    public function testBookTicket(): void
+    {
+        $clientStub = $this->createStub(Client::class);
+
+        $ticketMock = $this->createMock(Ticket::class);
+        $ticketMock->expects($this->atLeastOnce())->method('book');
+
+        $this->session->bookTicket($clientStub, $ticketMock);
+    }
+
+    public function testGetFreeTicket(): void
+    {
+        $this->assertInstanceOf(Ticket::class, $this->session->getFreeTicket());
+    }
+}
