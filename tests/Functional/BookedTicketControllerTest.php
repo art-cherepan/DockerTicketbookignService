31909318bde8287b2cf305d449115eee1a8cd8a3
artem <art.cherepan@gmail.com>
2022-04-08 16:48:34 +0900

    BookTicketCommand will be tested.

--- tests/Functional/BookedTicketControllerTest.php
+++ tests/Functional/BookedTicketControllerTest.php
@@ -2,14 +2,52 @@
 
 namespace App\Tests\Functional;
 
+use App\Domain\Booking\Entity\Session;
+use App\Fixture\SessionFixture;
+use Doctrine\ORM\EntityManagerInterface;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BookedTicketControllerTest extends WebTestCase
 {
+    private AbstractDatabaseTool $databaseTool;
+    private EntityManagerInterface $entityManager;
+    private KernelBrowser $client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
+        $this->entityManager = self::getContainer()->get(EntityManagerInterface::class);
+    }
+
     public function testMainPageIsAvailable(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/');
+        $this->client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testBookTicket(): void
+    {
+        $this->databaseTool->loadFixtures(
+            [SessionFixture::class],
+        );
+
+        $session = $this->entityManager->getRepository(Session::class)->findOneBy(['filmName' => 'Веном 1']);
+        $sessionId = $session->getId();
+
+        $this->client->request('GET', 'http://localhost/');
+        $this->client->submitForm('book_session_form[submit]', [
+            'book_session_form[clientName]' => 'Иван',
+            'book_session_form[clientPhoneNumber]' => '1234567899',
+            'book_session_form[session]' => $sessionId,
+        ]);
+
         $this->assertResponseIsSuccessful();
     }
 }
