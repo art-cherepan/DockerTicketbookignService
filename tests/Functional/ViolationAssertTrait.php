73bc44be2c0882f7a254d9c01075160dee4b1efe
artem <art.cherepan@gmail.com>
2022-04-12 18:46:52 +0900

    Add trait for custom assert in tests.

---
+++ tests/Functional/ViolationAssertTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use PHPUnit\Framework\Assert;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+trait ViolationAssertTrait
+{
+    protected function assertPropertyIsInvalid(string $propertyPath, string $expectedErrorMessage, ConstraintViolationListInterface $violations): void
+    {
+        $actualErrorMessage = self::findViolationMessagesByProperty($propertyPath, $violations);
+        Assert::assertContains(
+            $expectedErrorMessage,
+            $actualErrorMessage,
+            sprintf(
+                'Searching the error message after validation for the "%s in [%s]"',
+                $propertyPath,
+                implode(', ', $actualErrorMessage),
+            ),
+        );
+    }
+
+    /**
+     * @return array<string>
+     */
+    private static function findViolationMessagesByProperty(string $propertyPath, ConstraintViolationListInterface $violations): array
+    {
+        $violationMessages = [];
+
+        foreach ($violations as $violation) {
+            assert($violation instanceof ConstraintViolationInterface);
+
+            if ($violation->getPropertyPath() !== $propertyPath) {
+                continue;
+            }
+
+            $violationMessages[] = $violation->getMessage();
+        }
+
+        return $violationMessages;
+    }
+}
