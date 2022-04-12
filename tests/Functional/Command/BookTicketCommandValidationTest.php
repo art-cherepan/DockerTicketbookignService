530430eafb8e8b1209c787e2a876404051736452
artem <art.cherepan@gmail.com>
2022-04-12 18:49:28 +0900

    Add tests for command.

---
+++ tests/Functional/Command/BookTicketCommandValidationTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Tests\Functional\Command;
+
+use App\Domain\Booking\Command\BookTicketCommand;
+use App\Tests\Functional\ViolationAssertTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final class BookTicketCommandValidationTest extends WebTestCase
+{
+    use ViolationAssertTrait;
+
+    private BookTicketCommand $command;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->command = new BookTicketCommand();
+    }
+
+    public function testValidClientName(): void
+    {
+        $this->command->clientName = 'Андрей';
+        $validator = $this->getContainer()->get(ValidatorInterface::class);
+
+        $violations = $validator->validate($this->command);
+
+        self::assertEquals(0, count($violations));
+    }
+
+    public function testInvalidClientName(): void
+    {
+        $this->command->clientName = 'Fake name';
+        $validator = $this->getContainer()->get(ValidatorInterface::class);
+
+        $violations = $validator->validate($this->command);
+
+        $this->assertPropertyIsInvalid('clientName', 'The name must contain only Russian letters.', $violations);
+    }
+
+    public function testValidClientPhone(): void
+    {
+        $this->command->clientPhoneNumber = '7894561237';
+        $validator = $this->getContainer()->get(ValidatorInterface::class);
+
+        $violations = $validator->validate($this->command);
+
+        self::assertEquals(0, count($violations));
+    }
+
+    public function testInvalidClientPhone(): void
+    {
+        $this->command->clientPhoneNumber = '123456789';
+        $validator = $this->getContainer()->get(ValidatorInterface::class);
+
+        $violations = $validator->validate($this->command);
+
+        $this->assertPropertyIsInvalid(
+            'clientPhoneNumber',
+            'The number must be 10 digits long and must not include the following characters: \'+\' \'-\' \'*\'',
+            $violations,
+        );
+    }
+}
