839c7c35966516a85bcd03803ffae519be227e5d
artem <art.cherepan@gmail.com>
2022-04-14 11:48:12 +0900

    Add repository for used it in BookedTicketCommandHandlerTest

---
+++ src/Domain/Booking/Repository/BookedTicketRecordRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domain\Booking\Repository;
+
+use App\Domain\Booking\Entity\Client;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class BookedTicketRecordRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Client::class);
+    }
+}
